2dec3327c740df2341c2752871bd06f73e084901
Şeyma Ekici <100785004+doyochi-thebunny@users.noreply.github.com>
2023-02-18 13:58:10 +0300

    Initial commit

---
+++ Main.java
@@ -0,0 +1,19 @@
+public class Main{
+    public static void main(String[] args) {
+        
+        System.out.println("Prime numbers are: ");
+        int counter = 0;
+
+        for(int i = 1 ; i <= 100 ; i++){
+            for(int j = 1 ; j < i ; j++){
+                if((i % j) == 0){
+                    counter++;
+                }
+            }
+            if(counter < 2){
+                System.out.print(i + ", ");
+            }
+            counter = 0;
+        }
+    }
+}
